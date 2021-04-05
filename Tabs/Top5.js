faf7a8081373a431d7a4838dc59aa2fa5f845030
musta10 <mustafaenfadal@gmail.com>
2021-04-05 16:30:12 +0200

    paginatrap

--- Tabs/Top5.js
+++ Tabs/Top5.js
@@ -1,27 +1,8 @@
-import React, {useCallback }from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Alert, Button, Linking, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const supportedURL = "https://youtube.com/c/MykeTowers";
-
-
-const OpenURLButton = ({ url, children }) => {
-  const handlePress = useCallback(async () => {
-    // Checking if the link is supported for links with custom URL scheme.
-    const supported = await Linking.canOpenURL(url);
-
-    if (supported) {
-      // Opening the link with some app, if the URL scheme is "http" the web link should be opened
-      // by some browser in the mobile
-      await Linking.openURL(url);
-    } else {
-      Alert.alert(`Don't know how to open this URL: ${url}`);
-    }
-  }, [url]);
-
-  return <Button title={children} onPress={handlePress} />;
-};
 
 class Top5 extends React.Component {
     render(){
@@ -32,7 +13,6 @@ class Top5 extends React.Component {
       <MaterialCommunityIcons name="music" color={'#FFFFFF'} size={38} />
       <StatusBar style="auto" />
       </View>
-      <OpenURLButton url={supportedURL}>Myke Towers</OpenURLButton>
     </View>
         )
     }
--- Tabs/Trap.js
+++ Tabs/Trap.js
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, {useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, Linking, StyleSheet, Text, View } from 'react-native';
+
+
+const mykeTowers = "https://youtube.com/c/MykeTowers";
+
+
+const OpenURLButton = ({ url, children }) => {
+  const handlePress = useCallback(async () => {
+    // Checking if the link is supported for links with custom URL scheme.
+    const supported = await Linking.canOpenURL(url);
+
+    if (supported) {
+      // Opening the link with some app, if the URL scheme is "http" the web link should be opened
+      // by some browser in the mobile
+      await Linking.openURL(url);
+    } else {
+      Alert.alert(`Don't know how to open this URL: ${url}`);
+    }
+  }, [url]);
+
+  return <Button title={children} onPress={handlePress} />;
+};
+
+
 
 const Trap = () => {
   return(
     <View style={styles.container}>
         <Text>Pagina artistas Trap</Text>
         <StatusBar style="auto" />
+        <OpenURLButton url={mykeTowers}>
+         Myke Towers
+         </OpenURLButton>
       </View>
+       
           )
 }
 
-
-// class Trap extends React.Component {
-//     render(){
-//       const linkTo = useLinkTo();
-//         return(
-//   <View style={styles.container}>
-//       <Text>Pagina artistas Trap</Text>
-//       <Button onPress={() => linkTo('/profile/jane')}>
-//       Go to Jane's profile
-//     </Button>
-//       <StatusBar style="auto" />
-//     </View>
-//         )
-//     }
-// }
 
 const styles = StyleSheet.create({
     container: {
