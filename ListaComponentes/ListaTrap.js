646a72777b520f232c95e008cf99007be1de342f
musta10 <mustafaenfadal@gmail.com>
2021-04-08 21:07:32 +0200

    jsons

--- ListaComponentes/ListaTrap.js
+++ ListaComponentes/ListaTrap.js
@@ -1,35 +1,25 @@
-import React from "react";
-import { SafeAreaView, View, StyleSheet, Text, Linking } from "react-native";
+import React, {useState} from "react";
+import { SafeAreaView, View, StyleSheet, Text, Linking,TouchableOpacity } from "react-native";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import trap from "../listaJson/artistasTrap.json"
+
 
 const ListaTrap = () => {
+    const [trap, setTrap] = useState(trap)
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
-        <View style={styles.artistalist} onPress={() =>{
-         Linking.openURL("https://youtube.com/");
+        <TouchableOpacity  style={styles.artistalist} onPress={() =>{
+         Linking.openURL("https://youtube.com/c/MykeTowers");
         }}>
          <MaterialCommunityIcons name="crown" color={"black"} size={30} />
-         <Text style={styles.hyperlinkStyle}>Test link</Text>
-
-        </View>
-
-        <MaterialCommunityIcons name="crown" color={"black"} size={30} />
-        <Text
-          style={styles.hyperlinkStyle}
-          onPress={() => {
-            Linking.openURL("https://youtube.com/c/MykeTowers");
-          }}
-        >
-          Myke Towers
-        </Text>
-        
+         <Text style={styles.hyperlinkStyle}> Myke Towers</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 };
 
-export default ListaTrap;
 
 const styles = StyleSheet.create({
   container: {
@@ -41,7 +31,7 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: "#FFD321",
     alignItems: "center",
-    height: 60,
+    height: 65,
     justifyContent: "center",
     borderRadius: 35
   },
@@ -49,6 +39,8 @@ const styles = StyleSheet.create({
     color: "black",
     fontSize: 16,
     textTransform: "uppercase",
-    letterSpacing: 2
+    letterSpacing: 2,
   },
 });
+
+export default ListaTrap;
--- Tabs/Trap.js
+++ Tabs/Trap.js
@@ -5,43 +5,16 @@ import ListaArtistas from "../ListaComponentes/ListaTrap"
 
 
 
-const OpenURLButton = ({ url, children }) => {
-  const handlePress = useCallback(async () => {
-    // Checking if the link is supported for links with custom URL scheme.
-    const supported = await Linking.canOpenURL(url);
-
-    if (supported) {
-      // Opening the link with some app, if the URL scheme is "http" the web link should be opened
-      // by some browser in the mobile
-      await Linking.openURL(url);
-    } else {
-      Alert.alert(`Don't know how to open this URL: ${url}`);
-    }
-  }, [url]);
-
-  return <Button title={children} onPress={handlePress} />;
-};
-
-const mykeTowers = "https://youtube.com/c/MykeTowers";
-
-
 const Trap = () => {
   return(
-    <>
     <View style={styles.container}>
       <View style={styles.textos}>
       <Text style={styles.titulo}>Quien es el Rey del Trap ?</Text>
       <Text style={styles.subtitulo}>21 artistas cantan Trap</Text>
       </View>
         <StatusBar style="auto" />
-        {/* <View>
-          <OpenURLButton url={mykeTowers}>
-        Myke Towers
-        </OpenURLButton></View> */}
         <ListaArtistas />
       </View>
-       
-       </>
           )
 }
 
@@ -57,7 +30,8 @@ const styles = StyleSheet.create({
     },
     titulo: {
       fontSize: 20,
-      //color: "#FFD321",
+      color: "#000000",
+      
       fontWeight: "bold",
       textAlign: 'center',
     },
