49991dc5504525aa7c46589f07fa6517fb9005da
musta10 <mustafaenfadal@gmail.com>
2021-04-12 22:54:52 +0200

    listaanuelsolo

--- ListaComponentes/PlayList.js
+++ ListaComponentes/PlayList.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 class Playlist extends React.Component {
     render(){
         return(
-  <View style={styles.container}>
-      <Text style={styles.subtitulo}>Playlist de el verdadero AnuelAA</Text>
+  <View style={{height: 130, marginTop: 20}}>
+      <ScrollView>
+          <View style={{height: 100, width:100, marginLeft:20, borderWidth: 0.5, borderColor: "#DDDDDD"}}>
+            <View style={{flex: 2}}>
+            <Image source={require('../img/portadaplaylist.png')} style={{flex: 1, width: null, height: null, resizeMode: "cover"}} />
+            </View>
+            <View style={{flex: 1, paddingLeft: 10, paddingTop: 10}}>
+                <Text>Playlist de el verdadero AnuelAA</Text>
+            </View>
+          </View>
+      </ScrollView>
      
     </View>
         )
@@ -14,14 +24,7 @@ class Playlist extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-     flex: 0.6,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderWidth: 1,
-      borderColor: "#000",
-      backgroundColor: '#FFFF',
-    }
+    
     
   });
   
