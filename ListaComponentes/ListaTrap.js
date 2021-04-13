e02f24354d6da82f2f4affc07ac67beeb3fb458a
musta10 <mustafaenfadal@gmail.com>
2021-04-13 13:01:16 +0200

    colorsdiv

--- ListaComponentes/ListaTrap.js
+++ ListaComponentes/ListaTrap.js
@@ -1,24 +1,29 @@
 import React, {useState} from "react";
-import { SafeAreaView, View, StyleSheet, Text, Linking, TouchableOpacity } from "react-native";
+import { SafeAreaView, View, StyleSheet, Text, Linking, TouchableNativeFeedback } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import trapjson from "../listaJson/artistasTrap.json"
 
 const ListaTrap = () => {
+  const [rippleColor, setRippleColor] = useState(randomHexColor());
+  const [rippleOverflow, setRippleOverflow] = useState(false);
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#FFFFFF", flexDirection: "row"}}>
       <ScrollView>
       <View style={styles.container}>
         {trapjson.map((artista, key) => {
           return (
-            <TouchableOpacity key={key} onPress={() =>{
+            <TouchableNativeFeedback key={key} onPress={() =>{
               Linking.openURL(artista.url);
-             }}>
+              setRippleColor(randomHexColor());
+          setRippleOverflow(!rippleOverflow);
+             }}background={TouchableNativeFeedback.Ripple(rippleColor, rippleOverflow)}
+             >
                <View style={styles.artistalist}>
               <MaterialCommunityIcons name="crown" color={"black"} size={30} />
               <Text style={styles.hyperlinkStyle}> {artista.name} </Text>
               </View>
-             </TouchableOpacity>
+             </TouchableNativeFeedback>
      
           )
         })}
@@ -28,6 +33,11 @@ const ListaTrap = () => {
   );
 };
 
+const randomHexColor = () => {
+  return "#000000".replace(/0/g, function() {
+    return (~~(Math.random() * 16)).toString(16);
+  });
+};
 
 const styles = StyleSheet.create({
   container: {
